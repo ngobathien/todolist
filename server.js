7e93c8152d6eb7641004788a933188466e4444d3
nbathien <thienngo533@gmail.com>
2025-08-25 23:49:33 +0700

    Issue #2: API backend cho Todo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let todos = [];
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/api/todos", (req, res) => {
+  const todo = { id: Date.now(), text: req.body.text };
+  todos.push(todo);
+  res.json(todo);
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"))
