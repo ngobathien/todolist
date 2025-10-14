378845da05afb9c8942687adc20d60ee1e347b3f
ngobathien <bathienba1@gmail.com>
2025-10-15 00:23:14 +0700

    feat: add file todoController.js

---
+++ backend/src/controllers/todoController.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const getAllTodos = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export const createTodo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export const updateTodo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export const deletTodo = async (req, res) => {
+  try {
+  } catch (error) {}
+};
